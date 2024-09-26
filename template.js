633b321c8319789905a8c860ad44e44e1e3c62e1
rakibul972500 <rakibul972500@gmail.com>
2024-09-26 11:00:48 +0600

    use of back tic button

---
+++ template.js
@@ -0,0 +1,8 @@
+const x='Kumro';
+const y='Potash';
+const a=38;
+const b=23;
+// const result= 'salary of' + ' '  + x  + 'and' + y + ' '+ 'is:' + (a+b) +' Million';
+
+const result=`salary of ${x} and ${y} is: ${a+b} Poisa Rn`
+console.log(result);
