343ca0be1ad30f7bc65e5220adde181c729f8af6
rakibul972500 <rakibul972500@gmail.com>
2024-10-16 11:32:54 +0600

    spread operator object

--- spread.js
+++ spread.js
@@ -1,3 +1,19 @@
 var numbers=[1, 3, 5];
 var numbersNew=[...numbers, 8, 9, 0];
-console.log(numbersNew);
+// console.log(numbersNew);
+
+// spread operator for object
+
+const myObj1={
+    x:4,
+    y:5
+}
+const myObj2={
+    a:6,
+    b:7
+}
+
+console.log({
+    ...myObj1,
+    ...myObj2
+})
